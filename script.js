15006a51b62c5912f1f4be91bd94eae332266cbe
Manas Pradhan <manasmpradhan5@gmail.com>
2022-01-22 01:55:24 +0530

    created the algorithm

---
+++ script.js
@@ -0,0 +1,20 @@
+// create a variable 'play' that will hold player input
+// prompt player to make a play in rock paper scissors and store in 'play'
+// convert all characters in 'play' to lowercase
+// call function computerPlay to get random computer play and store in new variable 'cPlay'
+// call function playGame and log the result in the console 
+// create a function playGame that takes two inputs for plays from player and computer as playerSelection and computerSelection
+// if 'play' contains rock and 'computerPlay' contains scissors return 'You Win! Rock beats Scissors'
+// else if 'play' contains rock and 'computerPlay' contains rock return 'Tie!'
+// else if 'play' contains rock and 'computerPlay' contains paper return 'You Lose! Paper beats Rock'
+// else if 'play' contains paper and 'computerPlay' contains rock return 'You Win! Paper beats Rock'
+// else if 'play' contains paper and 'computerPlay' contains paper return 'Tie!'
+// else if 'play' contains paper and 'computerPlay' contains scissors return 'You Lose! Scissors beat Paper'
+// else if 'play' contains scissors and 'computerPlay' contains paper return 'You Win! Scissors beats Paper'
+// else if 'play' contains scissors and 'computerPlay' contains scissors return 'Tie!'
+// else if 'play' contains scissors and 'computerPlay' contains rock return 'You Lose! Rock beats Scissors'
+// create a function computerPlay that randomly generates 'rock', 'paper' or scissors'
+// randomly generate a number between 0 and 2 and store in new variable 'randPlay'
+// if 'randPlay' is 0, return 'rock'
+// else if 'randPlay' is 1, return 'paper'
+// else return 'scissors'
